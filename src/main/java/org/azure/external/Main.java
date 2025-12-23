bfd71dd9b7ccf2ab9348b91c14e53e3dce855a00
amruthareddyneelam-ammu <amruthareddyneelam@gmail.com>
2025-12-23 09:55:23 +0530

    PackageChangedToExternal

--- src/main/java/org/azure/external/Main.java
+++ src/main/java/org/azure/external/Main.java
@@ -1,4 +1,4 @@
-package org.azure.internal;
+package org.azure.external;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
